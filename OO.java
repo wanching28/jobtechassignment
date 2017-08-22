c056cf855242c32eaa49fbb0a9cdca0530506d83
wanching28 <ariel4eva@hotmail.com>
2017-08-22 21:15:08 +0800

    Q5-OO.java
    Design a new class and write a service method for user object

---
+++ OO.java
@@ -0,0 +1,11 @@
+(1) //Design a ‘user’ class based on the table above. 
+public class User {
+private int user_id;
+private String email_id;
+private String password;	
+private String first_name;
+private String last_name;
+private Boolean enabled;
+}
+
+(2)   //A service method ‘fetchUserInfoById’ method that takes user_id parameter and return user object
