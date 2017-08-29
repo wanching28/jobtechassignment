d02adcad735dad8554ab1045e19d58df9496ab1a
wanching28 <ariel4eva@hotmail.com>
2017-08-29 09:14:02 +0800

    Update myJavaApplication.java

--- myJavaApplication.java
+++ myJavaApplication.java
@@ -1,10 +1,10 @@
 // (1) Design a ‘user’ class based on the table above. 
 public class User {
-        private int user_id = 0;
-        private String email_id = "";
+        private int userId = 0;
+        private String emailId = "";
         private String password = "";	
-        private String first_name = "";
-        private String last_name = "";
+        private String firstName = "";
+        private String lastName = "";
         private boolean enabled;
     
 //Default constructor
@@ -12,22 +12,54 @@ public User(){
     enabled = false;
 }
     
+public User (int userId, String emailId, String password,
+String firstName, String lastName, boolean enabled)
+{
+        this.userId = userId;
+        this.emailId = emailId;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.enabled = enabled;
+}
+        
+public int getUserId(){
+        return userId;
+}
+
+public String getEmailId(){
+        return emailId;
+}
+
+public String getPassword(){
+        return password;
+}
+
+public String getFirstName(){
+        return firstName;
+}
+
+public String getLastName(){
+        return lastName;
+}
+
+public boolean getEnabled(){
+        return enabled;
+}
+        
 // (2) A service method that takes user_id parameter and return user object
 User fetchUserInfoById (int user_id) {
         User user = new User();
-        user = fetchUserInfo(user_id);
+        try {
+                user = fetchUserInfo(user_id);
+                return user;
+        }
+        catch (IOException e) {
+                
+        }
         return user;
-    }
 }
 
-public class MyJavaApplication {
-    
-    public static void main(String[] args)
-    {
-            int userid = 101;
-            User user = new User();
-            user = user.fetchUserInfoById(userid);
-    }
 }
 
 
