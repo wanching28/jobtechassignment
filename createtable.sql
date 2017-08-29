018fe3ed5b6e8c2f082f07962ce01d8e1146fb75
wanching28 <ariel4eva@hotmail.com>
2017-08-29 09:37:04 +0800

    Update createtable.sql

--- createtable.sql
+++ createtable.sql
@@ -1,18 +1,17 @@
 CREATE TABLE users (
-user_id 	INT(11)  	NOT NULL   AUTO_INCREMENT,
-email_id  	VARCHAR(200)	NOT NULL, 
-password 	VARCHAR(64) 	NOT NULL, 
-first_name 	VARCHAR(128) 	NOT NULL, 
-last_name 	VARCHAR(128) 	NOT NULL,
-enabled 	BIT		DEFAULT 1,
-PRIMARY KEY (user_id)
+user_id INT identity(1, 1) PRIMARY KEY,
+email_id NVARCHAR(200) NOT NULL DEFAULT '',
+password NVARCHAR(64) NOT NULL DEFAULT '',
+first_name NVARCHAR(128) NOT NULL DEFAULT '',
+last_name NVARCHAR(128) NOT NULL DEFAULT '',
+enabled BIT DEFAULT 1
 );
 
 CREATE TABLE user_singin_logs (
-user_id 		INT(11) 		NOT NULL, 
-session_id 		VARCHAR(128) 	NOT NULL,
-signed_in_time 	TIMESTAMP,
-signed_out_time 	TIMESTAMP,
-PRIMARY KEY (user_id, session_id)
+user_id INT	NOT NULL, 
+session_id NVARCHAR(128) 	NOT NULL DEFAULT '',
+signed_in_time  DATETIME NOT NULL DEFAULT '0000-00-00 00:00:00', 
+signed_out_time DATETIME NOT NULL DEFAULT '0000-00-00 00:00:00',
+PRIMARY KEY (user_id, session_id),
 FOREIGN KEY (user_id) REFERENCES users (user_id)
 );
